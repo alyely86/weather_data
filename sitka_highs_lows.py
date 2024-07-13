d45eae6e319ed774444392c3ed69b2ac6436a3b8
aly <alyasyly868@gmail.com>
2024-07-12 19:07:46 -0700

    Categorizing and creating a Python file named death_valley _highs_lows.py

--- sitka_highs_lows.py
+++ sitka_highs_lows.py
@@ -3,7 +3,7 @@ import csv
 import matplotlib.pyplot as plt
 from datetime import datetime
 
-path = Path('weather_data/sitka_weather_2021_simple.csv')
+path = Path('weather_data/data/sitka_weather_2021_simple.csv')
 lines = path.read_text().splitlines()
 
 
