864027e7a953117dc9e1d3c33ed02413cd18e873
aly <alyasyly868@gmail.com>
2024-07-12 19:21:14 -0700

    Error Checking

--- death_valley _highs_lows.py
+++ death_valley _highs_lows.py
@@ -3,7 +3,7 @@ import csv
 import matplotlib.pyplot as plt
 from datetime import datetime
 
-path = Path('weather_data/data/sitka_weather_2021_simple.csv')
+path = Path('weather_data/data/death_valley_2021_simple.csv')
 lines = path.read_text().splitlines()
 
 
@@ -14,11 +14,15 @@ header_row = next(reader)
 dates,highs,lows = [],[],[]
 for row in reader:
     current_date = datetime.strptime(row[2],'%Y-%m-%d')
-    high = int(row[4])
-    low = int(row[5])
-    dates.append(current_date)
-    highs.append(high)
-    lows.append(low)
+    try:
+        high = int(row[3])
+        low = int(row[4])
+    except ValueError:
+        print(f"Missing data for {current_date}")
+    else:
+        dates.append(current_date)
+        highs.append(high)
+        lows.append(low)
 
 # Plot the high and low temperatures. 
 plt.style.use('seaborn-v0_8')
@@ -28,7 +32,8 @@ ax.plot(dates,lows , color = 'blue',alpha = 0.5)
 ax.fill_between(dates,highs,lows,facecolor='blue', alpha=0.1)
 
 # Format plot.
-ax.set_title("Daily High and Low Temperatures, 2021", fontsize=24)
+title = "Daily High and Low Temperatures, 2021\nDeath Valley, CA"
+ax.set_title(title, fontsize=24)
 ax.set_xlabel('',fontsize = 16)
 fig.autofmt_xdate()
 ax.set_ylabel("Temperature (F)",fontsize = 16)
